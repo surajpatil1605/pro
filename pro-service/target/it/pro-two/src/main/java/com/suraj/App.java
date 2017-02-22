d6478179a53a0b5c36363e3376729d9e2add35e2
surajpatil1605 <suraj@BRrpp658sdc.local>
2017-02-22 17:52:35 +0530

    files added

---
+++ pro-service/target/it/pro-two/src/main/java/com/suraj/App.java
@@ -0,0 +1,13 @@
+package com.suraj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
